2dc0e2bd1651449bc9cb76cade72f73273b6475f
Samvel643 <samvelfahradyan9@gmail.com>
2026-09-10 21:42:01 +0400

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,14 @@
+def custom_sort(a):
+    for i in range(1, len(a)):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > key:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+    return a
+
+
+nums = [-61, 0, 124, -13, 110]
+s = custom_sort(nums)
+print(s)
